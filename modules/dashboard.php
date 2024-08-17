8ca9cba425f5c788dac8584baf43dde3ed1ecf11
SU0119SU <suyamaomao@gmail.com>
2024-08-17 14:42:13 +0800

    將content/sidebar/header獨立，並刪減html部分

--- modules/dashboard.php
+++ modules/dashboard.php
@@ -8,18 +8,12 @@
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
     <?php include("../css/css.php") ?>
 </head>
 <body>
 <?php include("../modules/dashboard-header.php");?>
+<?php include ("../modules/dashboard-sidebar.php");?>
 <?php include("../modules/dashboard-content.php");?>
-<?php include("./javascript/script.js");?>
+<?php include("../javascript/script.js");?>
     </body>
 </html>
