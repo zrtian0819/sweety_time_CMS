47998da27d99507db1714bbadbbefb9fc17907b8
TzuuChen <jf76501@gmail.com>
2024-08-18 14:35:21 +0800

    將版面更改為小天的樣式 & 把資料倒出來

--- page/lesson.php
+++ page/lesson.php
@@ -17,19 +17,35 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Lesson</title>
-    <?php include("../css/css.php"); ?>
+    <?php include("../css/css_Joe.php"); ?>
 </head>
 
 <body>
-    <?php include("../modules/dashboard-header.php"); ?>
-    <?php include("../modules/dashboard-sidebar.php"); ?>
-
-    <!-- Content -->
-    <div class="col-lg-9 col-md-9"> <!-- 這一層佈局不要動 -->
-        <div class="content neumorphic">
-            <?php foreach ($rows as $row): ?>
-                <h1><?= $row["name"] ?></h1>
-            <?php endforeach; ?>
+    <?php include("../modules/dashboard-header_Joe.php"); ?>
+    <div class="container-fluid d-flex flex-row px-4">
+        <?php include("../modules/dashboard-sidebar_Joe.php"); ?>
+        <div class="main col neumorphic p-5">
+            <!-- Content -->
+            <table class="table">
+                <thead class="text-center">
+                    <th>課程編號</th>
+                    <th>課程名稱</th>
+                    <th>授課老師</th>
+                    <th>課程人數</th>
+                    <th>詳細資訊</th>
+                </thead>
+                <?php foreach ($rows as $row): ?>
+                    <tbody>
+                        <tr class="text-center">
+                            <td><?= $row["lesson_id"] ?></td>
+                            <td><?= $row["name"] ?></td>
+                            <td><?= $row["teacher_id"] ?></td>
+                            <td><?= $row["quota"] ?></td>
+                            <td><a href="" class="btn"><i class="fa-regular fa-eye"></i></a></td>
+                        </tr>
+                    </tbody>
+                <?php endforeach; ?>
+            </table>
         </div>
     </div>
 </body>
