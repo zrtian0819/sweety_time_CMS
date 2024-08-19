4ab8db45279e707ce2cfda3784d1a852e8ef297c
Joe <cjt3399@gmail.com>
2024-08-19 10:15:02 +0800

    加入一些api和function

--- function/login_status_inspect.php
+++ function/login_status_inspect.php
@@ -1,11 +1,10 @@
-<!-- 
-檢查登入狀態；
-引入方式:
-require_once("function/login_status_inspect.php");
--->
-
+<!-- 檢查登入狀態；-->
 <?php
 
-
+// 若session中沒有user則導頁回登入畫面
+if(!isset($_SESSION["user"])){
+    header("location: ../page/login.php");
+    exit;
+}
 
 ?>
