bc6e074374328de0f9de774dc9661841cabe1257
Joe_Zhang <cjt3399@gmail.com>
2024-08-19 23:04:05 +0800

    新增不同角色登入時sideBar選項顯示隨之不同的設定

--- modules/dashboard-sidebar_Joe.php
+++ modules/dashboard-sidebar_Joe.php
@@ -1,47 +1,64 @@
  <!-- Sidebar -->
+ <?php
+
+    // session_start();
+    if (isset($_SESSION["user"])) {
+        $role = $_SESSION["user"]["role"];
+        $userId = $_SESSION["user"]["user_id"];
+    } else {
+        $role = "";
+        $userId = "";
+    }
+
+    ?>
 
  <div class="sidebar neumorphic setting" id="sideBar">
-     <button id="adminBtn" class="btn btn-neumorphic toggle-btn">
-         <h5 class="h5">admin管理</h5>
-     </button>
-     <ul id="adminList" class="nav flex-column align-items-center">
-         <li class="nav-item">
-             <a class="nav-link" href="#">會員管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">文章管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">師資管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">課程管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">優惠券管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">總商家管理</a>
-         </li>
-     </ul>
 
-     <button id="storeBtn" class="btn btn-neumorphic toggle-btn">
-         <h5 class="h5">店家管理</h5>
-     </button>
-     <ul id="storeList" class="nav flex-column align-items-center">
-         <li class="nav-item">
-             <a class="nav-link" href="#">店家基本資料</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">商品管理</a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link" href="#">訂單管理</a>
-         </li>
-     </ul>
+     <?php if ($role == "admin"): ?>
+         <button id="adminBtn" class="btn btn-neumorphic toggle-btn">
+             <h5 class="h5">admin管理</h5>
+         </button>
+         <ul id="adminList" class="nav flex-column align-items-center">
+             <li class="nav-item">
+                 <a class="nav-link" href="#">會員管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">文章管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">師資管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">課程管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">優惠券管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">總商家管理</a>
+             </li>
+         </ul>
+     <?php endif; ?>
 
-    <!-- 店家註冊並非在dashboard頁面執行 -->
-    <!--  
+     <?php if ($role == "shop" || $role == "admin"): ?>
+         <button id="storeBtn" class="btn btn-neumorphic toggle-btn">
+             <h5 class="h5">店家管理</h5>
+         </button>
+         <ul id="storeList" class="nav flex-column align-items-center">
+             <li class="nav-item">
+                 <a class="nav-link" href="shop-info.php">店家基本資料</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="product-list.php">商品管理</a>
+             </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="#">訂單管理</a>
+             </li>
+         </ul>
+     <?php endif; ?>
+
+     <!-- 店家註冊並非在dashboard頁面執行 -->
+     <!--  
      <button id="registerBtn" class="btn btn-neumorphic toggle-btn">
          <h5 class="h5">店家註冊</h5>
      </button>
--- page/product-list.php
+++ page/product-list.php
@@ -5,9 +5,9 @@ include("../function/function.php");
 
 if (isset($_GET["shopid"])) {
     $shopId = $_GET["shopid"];
-    $sql = "SELECT * FROM products WHERE id=$shopId AND available = 1 ORDER BY product_id";
+    $sql = "SELECT * FROM product WHERE id=$shopId AND available = 1 ORDER BY product_id";
 } else {
-    $sql = "SELECT * FROM products WHERE available = 1 ORDER BY product_id";
+    $sql = "SELECT * FROM product WHERE available = 1 ORDER BY product_id";
 }
 
 $result = $conn->query($sql);
@@ -15,7 +15,6 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 $productCount = $result->num_rows;
 
 // print_r($rows);
-
 
 ?>
 
@@ -32,6 +31,10 @@ $productCount = $result->num_rows;
     <style>
         .bdrs {
             border-radius: 20px;
+        }
+
+        .dontNextLine {
+            white-space: nowrap;
         }
     </style>
 </head>
@@ -63,12 +66,12 @@ $productCount = $result->num_rows;
                     <table class="table table-bordered table-hover bdrs">
                         <thead class="text-center table-dark">
                             <tr>
-                                <th>名稱</th>
-                                <th>照片</th>
-                                <th>價格</th>
-                                <th>描述</th>
-                                <th>庫存數量</th>
-                                <th>編輯</th>
+                                <th class="dontNextLine">名稱</th>
+                                <th class="dontNextLine">照片</th>
+                                <th class="dontNextLine">價格</th>
+                                <th class="dontNextLine">描述</th>
+                                <th class="dontNextLine">庫存數量</th>
+                                <th class="dontNextLine">編輯</th>
                             </tr>
                         </thead>
 
@@ -80,8 +83,8 @@ $productCount = $result->num_rows;
                                     <th class="text-center"><?= number_format($row["price"]) ?></th>
                                     <th><?= getLeftChar($row["description"], 100) . "..." ?></th>
                                     <th class="text-center"><?= $row["stocks"] ?></th>
-                                    <th><a href="/product.php?productid=<?= $row["product_id"] ?>" class="btn btn-warning">
-                                            <i class="fa-regular fa-pen-to-square"></i>
+                                    <th><a href="product.php?productId=<?= $row["product_id"] ?>" class="btn btn-warning">
+                                            <i class="fa-solid fa-list"></i>
                                         </a></th>
                                 </tr>
                             <?php endforeach; ?>
