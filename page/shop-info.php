5aa2c929e06d81ddd4aed93570d354e4794f9e71
Dennis <sfnandy65066506@gmail.com>
2024-08-21 11:55:27 +0800

    shop-info修改功能上線

--- page/shop-info.php
+++ page/shop-info.php
@@ -1,11 +1,13 @@
 <?php
 require_once("../db_connect.php");      //避免sidebar先載入錯誤,人天先加的
 
-// 獲取網址中的 shopId 參數
-// $shop_id = isset($_GET['shopId']) ? intval($_GET['shopId']) : 0;
-
-if (session_status() == PHP_SESSION_NONE) {
+if (session_status() == PHP_SESSION_NONE) {  //啟動session
     session_start();
+}
+
+if (!isset($_SESSION["shop"]["shop_id"])) {    //用 isset() 檢查$_SESSION為空的錯誤
+    header("location: dashboard-home_Joe.php");
+    exit;
 }
 
 $shop_id = $_SESSION["shop"]["shop_id"];
@@ -13,15 +15,14 @@ $shop_id = $_SESSION["shop"]["shop_id"];
 
 if(isset($_SESSION["shop"]["shop_id"])){
     
-    if($shop_id=="admin"){
+    if($shop_id == "admin"){
         header("location: dashboard-home_Joe.php");
     }
 
-    if(isset($_GET["shopId"])){
-        if( $_GET["shopId"]!=$_SESSION["shop"]["shop_id"] ){
-            header("location: shop-info.php?shopId=".$_SESSION["shop"]["shop_id"]);
-        }
-    }
+    if (isset($_GET["shopId"]) && $_GET["shopId"] != $shop_id) {    //防止用戶改網頁的id來進入別人的頁面
+        header("location: shop-info.php?shopId=$shop_id");
+        exit;
+    }    
 
 }else{
     header("location: dashboard-home_Joe.php");
@@ -30,35 +31,26 @@ if(isset($_SESSION["shop"]["shop_id"])){
 
 if ($shop_id > 0) {
     // 根據 shop_id 查詢店家資訊
-    $sql_shop_info = "SELECT * FROM shop WHERE shop_id = $shop_id";
+    $sql_shop_info = "  SELECT * 
+                        FROM shop
+                        JOIN shop_photo ON shop.shop_id = shop_photo.shop_id
+                        WHERE shop.shop_id = $shop_id";
+
+
     $result_shop_info = $conn->query($sql_shop_info);
 
     if ($result_shop_info->num_rows > 0) {
         $shop_info = $result_shop_info->fetch_assoc();
-        $name = $shop_info["name"];
+        $shop_name = $shop_info["name"];
         $phone = $shop_info["phone"];
         $address = $shop_info["address"];
         $description = $shop_info["description"];
         $sign_up_time = $shop_info["sign_up_time"];
         $latitude = $shop_info["latitude"];
         $longitude = $shop_info["longitude"];
+        $file_name = $shop_info["file_name"];
     } else {
         echo "找不到指定的店家";
-        exit;
-    }
-
-    // 根據 shop_id 查詢店家照片
-    $sql_shop_photo = "SELECT * FROM shop_photo WHERE shop_id = ?";
-    $stmt_shop_photo = $conn->prepare($sql_shop_photo);
-    $stmt_shop_photo->bind_param("i", $shop_id);
-    $stmt_shop_photo->execute();
-    $result_shop_photo = $stmt_shop_photo->get_result();
-
-    if ($result_shop_photo->num_rows > 0) {
-        $shop_photo = $result_shop_photo->fetch_assoc();
-        $file_name = $shop_photo["file_name"];
-    } else {
-        echo "找不到指定的店家照片";
         exit;
     }
 } else {
@@ -92,13 +84,12 @@ if ($shop_id > 0) {
                     <div class="col-12 col-md-6 col-lg-5 position-relative d-flex justify-content-center align-items-center mb-3 mb-md-0">
                         <a href="">
                             <img class="shop-info-logo" src="../images/shop_logo/<?=$file_name;?>" alt="店家Logo">
-                            <button class="btn btn-secondary change-image-btn">更換圖片</button>
                         </a>
                     </div>
                     <div class="col-12 col-md-6 col-lg-5 px-4 shop-info-detail">
                         <h3 class="mb-3">店家資訊</h3>
                         <ul class="list-unstyled">
-                            <li class="my-2">店名：<?= $name;?></li>
+                            <li class="my-2">店名：<?= $shop_name;?></li>
                             <li class="my-2">電話：<?= $phone;?></li>
                             <li class="my-2">地址：<?= $address;?></li>
                             <li class="my-2">註冊時間：<?= $sign_up_time;?></li>
@@ -117,8 +108,8 @@ if ($shop_id > 0) {
                             </ul>
                         </div>
                     </div>
-                    <div>
-                        <a href="shop-info-edit.php?id=<?=$row["shop_id"]?>" class="btn btn-primary"><i class="fa-solid fa-user-pen"></i></a>
+                    <div class="d-flex justify-content-end">
+                        <a href="shop-info-edit.php" class="btn btn-secondary"><i class="fa-solid fa-user-pen"></i></a>
                     </div>
                 </div>
             </div>
