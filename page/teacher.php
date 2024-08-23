8ea33e1b6d859f09ab4961f4c3d71a56335d1034
SU0119SU <suyamaomao@gmail.com>
2024-08-23 17:12:16 +0800

    刪除teacher.php裡面的teacher-style.css

--- page/teacher.php
+++ page/teacher.php
@@ -58,8 +58,6 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teacher</title>
     <?php include("../css/css_Joe.php"); ?>
-    <?php include("../css/teacher"); ?>
-
 </head>
 
 <body>
