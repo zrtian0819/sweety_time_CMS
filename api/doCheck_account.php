3d6c3e11bee8ab13b38f50283e178dfc4e6a5851
Dennis <sfnandy65066506@gmail.com>
2024-08-23 13:36:35 +0800

    新增create-shop.js/註冊新的商家功能完整版

---
+++ api/doCheck_account.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../db_connect.php");
+
+if(isset($_POST['account'])) {
+    $account = $_POST['account'];
+    
+    // 防止 SQL 注入
+    $account = mysqli_real_escape_string($conn, $account);
+    
+    $sql = "SELECT * FROM users WHERE account = '$account'";
+    $result = $conn->query($sql);
+    
+    if($result->num_rows > 0) {
+        echo "exists";
+    } else {
+        echo "not_exists";
+    }
+} else {
+    echo "error";
+}
+
+$conn->close();
+?>
