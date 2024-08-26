bfbf60bdf0fa40fe845899caf28fa1268198fbcf
Tzu <jf76501@gmail.com>
2024-08-26 10:11:30 +0800

    side bar改為Su的

--- page/lesson.php
+++ page/lesson.php
@@ -137,7 +137,7 @@ foreach ($rowsPro as $productClass) {
 <body>
     <?php include("../modules/dashboard-header_Joe.php"); ?>
     <div class="container-fluid d-flex flex-row px-4">
-        <?php include("../modules/dashboard-sidebar_Joe.php"); ?>
+        <?php include("../modules/dashboard-sidebar_Su.php"); ?>
         <div class="main col neumorphic p-5 pt-3">
             <div class="py-1">
                 <form action="" method="GET" class="d-flex flex-row">
