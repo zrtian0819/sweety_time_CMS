7c98ee3834021dfde55bc71dcdf9c9cbc753e35e
Joe <cjt3399@gmail.com>
2024-08-26 10:39:44 +0800

    將sidebar 訂單管理顯示修正

--- modules/dashboard-sidebar_Su.php
+++ modules/dashboard-sidebar_Su.php
@@ -45,9 +45,11 @@ function getBasePageName()
             <li class="nav-item">
                 <a class="nav-link <?= (getBasePageName() == 'product-list') ? 'active' : '' ?>" href="product-list.php">商品管理</a>
             </li>
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="order-list.php">訂單管理</a>
-            </li> -->
+            <?php if ($role == "admin"): ?>
+             <li class="nav-item">
+                 <a class="nav-link <?= (getBasePageName() == 'order-list') ? 'active' : '' ?>" href="order-list.php">訂單管理</a>
+             </li>
+            <?php endif; ?>
         </ul>
     <?php endif; ?>
 </div>
