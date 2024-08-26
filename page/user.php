6e3c09d2d311eb2f32422e05cfaf4fc8ccc120ee
Jolin <lonely507487@gmail.com>
2024-08-26 11:39:53 +0800

    user細節UI調整

--- page/user.php
+++ page/user.php
@@ -62,54 +62,53 @@ if ($userCount > 0) {
 
         <?php include("../modules/dashboard-sidebar_Joe.php"); ?>
 
-        <div class="container-fluid d-flex flex-row px-4">
-            <div class="main col neumorphic p-5">
-                <div class="d-flex">
-                    <a class="btn-animation btn btn-custom d-inline-flex flex-row align-items-center mb-3 mx-3" href="users.php">
-                        <i class="fa-solid fa-arrow-left-long"></i><span class="btn-animation-innerSpan d-inline-block">返回</span>
-                    </a>
-                    <h2 class="mb-3"><?= $title ?> 基本資料</h2>
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <?php if ($userCount > 0): ?>
+        <div class="main col neumorphic p-5">
+            <div class="d-flex">
+                <a class="btn-animation btn btn-custom d-inline-flex flex-row align-items-center mb-3 mx-3" href="users.php">
+                    <i class="fa-solid fa-arrow-left-long"></i><span class="btn-animation-innerSpan d-inline-block">返回</span>
+                </a>
+                <h2 class="mb-3"><?= $title ?> 基本資料</h2>
+            </div>
+            <div class="container-fluid d-flex justify-content-center">
+                <div class="row col-10">
+                    <?php if ($userCount > 0): ?>
 
-                            <div class="mb-3 d-flex justify-content-center align-items-center flex-column">
-                                <img src="<?= ($imagePath) ?>" alt="Profile Image" class="object-fit-fill user-img">
-                            </div>
-                            <table class="table table-bordered">
-                                <tr>
-                                    <th>User ID</th>
-                                    <td><?= $row["user_id"] ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Name</th>
-                                    <td><?= $row["name"] ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Password</th>
-                                    <td><?= $row["password"] ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Birthday</th>
-                                    <td><?= $row["birthday"] ?></td>
-                                </tr>
-                                <tr>
-                                    <th>Sign up time</th>
-                                    <td><?= $row["sign_up_time"] ?></td>
-                                </tr>
-                            </table>
-                            <div class="d-flex justify-content-end">
-                                <a href="user-edit.php?user_id=<?= $row["user_id"] ?>" class="btn btn-neumorphic user-btn"><i class="fa-solid fa-user-pen"></i></a>
-                            </div>
-                        <?php else: ?>
-                            使用者不存在
-                        <?php endif; ?>
+                        <div class="mb-3 d-flex justify-content-center align-items-center flex-column">
+                            <img src="<?= ($imagePath) ?>" alt="Profile Image" class="object-fit-cover my-5 ms-5 ps-5 user-img">
+                        </div>
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>User ID</th>
+                                <td><?= $row["user_id"] ?></td>
+                            </tr>
+                            <tr>
+                                <th>Name</th>
+                                <td><?= $row["name"] ?></td>
+                            </tr>
+                            <tr>
+                                <th>Password</th>
+                                <td><?= $row["password"] ?></td>
+                            </tr>
+                            <tr>
+                                <th>Birthday</th>
+                                <td><?= $row["birthday"] ?></td>
+                            </tr>
+                            <tr>
+                                <th>Sign up time</th>
+                                <td><?= $row["sign_up_time"] ?></td>
+                            </tr>
+                        </table>
+                        <div class="d-flex justify-content-end">
+                            <a href="user-edit.php?user_id=<?= $row["user_id"] ?>" class="btn btn-neumorphic user-btn"><i class="fa-solid fa-user-pen"></i></a>
+                        </div>
+                    <?php else: ?>
+                        使用者不存在
+                    <?php endif; ?>
 
-                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 
 
